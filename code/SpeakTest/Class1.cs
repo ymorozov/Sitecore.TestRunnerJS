1dc474816fd35ef83a2f41dba88cc91aa79af0ae
Yuriy Morozov <yum@sitecore.net>
2014-11-18 10:34:34 +0200

    Created an empty project

---
+++ code/SpeakTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeakTest
+{
+    public class Class1
+    {
+    }
+}
