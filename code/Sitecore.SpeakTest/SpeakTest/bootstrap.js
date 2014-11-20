5b06a86f01c537e94ed9335307f904ade1b6dbdd
Yuriy Morozov <yury.morozov@gmail.com>
2014-11-21 00:17:21 +0200

    Loading of JS test frameworks was added to bootstrap.

--- code/Sitecore.SpeakTest/SpeakTest/bootstrap.js
+++ code/Sitecore.SpeakTest/SpeakTest/bootstrap.js
@@ -1,1 +1,22 @@
-﻿alert('bootstrap loaded!');
+﻿(function () {
+  console.log("SpeakTest suite was loaded.");
+
+  var baseUrl = '/SpeakTest/assets';
+  require.config({
+    baseUrl: baseUrl,
+    paths: {
+      mocha: 'mocha',
+      chai: 'chai',
+      sinon: 'sinon',
+      tests: 'tests'
+    },
+  });
+
+  require(['require', 'chai', 'mocha', 'sinon'], function (require, chai) {
+    expect = chai.expect;
+    mocha.setup('bdd');
+
+    require(['/-/speak/v1/assets/main.js'], function () {
+    });
+  });
+})();
