4fe7faf68be242c1195c454b643215e9d72286e1
Yuriy Morozov <yury.morozov@gmail.com>
2014-11-20 23:33:08 +0200

    Can switch speak test and debug mode.

--- code/Sitecore.SpeakTest/Pipelines/HttpRequest/EnsureSpeakTestingEnabled.cs
+++ code/Sitecore.SpeakTest/Pipelines/HttpRequest/EnsureSpeakTestingEnabled.cs
@@ -8,21 +8,22 @@
 
   public class EnsureSpeakTestingEnabled : HttpRequestProcessor
   {
+    private object _previousValue;
+
     private const string bootstrapModulePath = "/SpeakTest/bootstrap.js";
 
     private const string requireJSSettingName = "Speak.Html.RequireJSBackwardCompatibilityFile";
-
-    private Hashtable GetSettings()
-    {
-      var type = typeof(Settings);
-      var info = type.GetField("settings", BindingFlags.NonPublic | BindingFlags.Static);
-      return (Hashtable)info.GetValue(null);
-    }
 
     public override void Process(HttpRequestArgs args)
     {
       if (WebUtil.GetQueryString("sc_speaktest") != "1")
       {
+        if (WebUtil.GetQueryString("sc_speaktest") == "0" && _previousValue != null)
+        {
+          this.GetSettings()[requireJSSettingName] = _previousValue;
+          Context.Diagnostics.Debugging = false;
+        }
+
         return;
       }
 
@@ -31,7 +32,17 @@
         return;
       }
 
-      this.GetSettings()[requireJSSettingName] = bootstrapModulePath;
+      var settings = this.GetSettings();
+      _previousValue = settings[requireJSSettingName];
+      settings[requireJSSettingName] = bootstrapModulePath;
+      Context.Diagnostics.Debugging = true;
+    }
+
+    private Hashtable GetSettings()
+    {
+      var type = typeof(Settings);
+      var info = type.GetField("settings", BindingFlags.NonPublic | BindingFlags.Static);
+      return (Hashtable)info.GetValue(null);
     }
   }
 }
