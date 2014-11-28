20f6d9504a3d839fed5003736685c87a7fce524a
Yuriy Morozov <yum@sitecore.net>
2014-11-28 17:10:27 +0200

    Run tests after speak debug output stopped to appear in console.

--- code/Sitecore.SpeakTest/SpeakTest/bootstrapper.js
+++ code/Sitecore.SpeakTest/SpeakTest/bootstrapper.js
@@ -1,7 +1,6 @@
 ﻿(function () {
   console.log("SpeakTest suite was loaded.");
 
-  var testsWasRun = false;
   var baseUrl = '/speaktest/assets/';
   require.config({
     paths: {
@@ -25,19 +24,66 @@
           return;
         }
 
-        $(document).ajaxStop(function () {
-          console.log('Page loading completed.');
+        var lastBindingSeenTime = 0;
+        var bindingSeenTimeDelta = 0;
+        var runnerIsStarted = false;
 
-          if (!testsWasRun) {
+        var taskId = 0;
+
+        function testRunner() {
+          bindingSeenTimeDelta = new Date().getTime() - lastBindingSeenTime;
+          
+          if (bindingSeenTimeDelta > 2000) {
+            window.clearInterval(taskId);
+            
+            console.log('Running tests.');
+
             if (window.mochaPhantomJS) {
               mochaPhantomJS.run();
             } else {
               mocha.reporter(mocha.WebKit);
               mocha.run();
             }
-            testsWasRun = true;
           }
-        });
+        }
+
+        function interceptConsole() {
+          var console = window.console;
+          if (!console) return;
+          function intercept(method) {
+            var original = console[method];
+            console[method] = function () {
+              var arg = arguments[0].toString();
+              if (arg.lastIndexOf("Applying Binding for the element", 0) === 0
+                || arg.lastIndexOf("Requiring files:", 0) === 0
+                || arg.lastIndexOf("Requiring page code:", 0) === 0
+                || arg.lastIndexOf("initialize -", 0) === 0) {
+                
+                lastBindingSeenTime = new Date().getTime();
+
+                if (!runnerIsStarted) {
+                  taskId = window.setInterval(testRunner, 100);
+                  runnerIsStarted = true;
+                }
+              }
+
+              if (original.apply) {
+                // Do this for normal browsers
+                original.apply(console, arguments);
+              } else {
+                // Do this for IE
+                var message = Array.prototype.slice.apply(arguments).join(' ');
+                original(message);
+              }
+            };
+          }
+
+          var methods = ['log', 'warn', 'error'];
+          for (var i = 0; i < methods.length; i++)
+            intercept(methods[i]);
+        }
+
+        interceptConsole();
       });
     });
   });
