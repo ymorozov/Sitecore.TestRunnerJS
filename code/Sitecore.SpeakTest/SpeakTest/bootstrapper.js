08d1a319a5c2293b2dc20424a17a61d7c3fa970c
Yuriy Morozov <yury.morozov@gmail.com>
2014-12-20 20:11:54 +0200

    Fixed concurrency issue in bootstrapper on mocha run.

--- code/Sitecore.SpeakTest/SpeakTest/bootstrapper.js
+++ code/Sitecore.SpeakTest/SpeakTest/bootstrapper.js
@@ -18,75 +18,77 @@
     expect = chai.expect;
     mocha.setup('bdd');
 
+    var taskId = 0,
+        runnerCheckerIsStarted = false,
+        lastBindingSeenTime = 0,
+        bindingSeenTimeDelta = 0,
+        testRunner = null;
+
+    function runnerChecker() {
+      bindingSeenTimeDelta = new Date().getTime() - lastBindingSeenTime;
+
+      if (bindingSeenTimeDelta > 2000 && testRunner != null) {
+        window.clearInterval(taskId);
+        testRunner();
+      }
+    }
+
+    function interceptConsole() {
+      var console = window.console;
+      if (!console) return;
+      function intercept(method) {
+        var original = console[method];
+        console[method] = function () {
+          var arg = arguments[0].toString();
+          if (arg.lastIndexOf("Applying Binding for the element", 0) === 0
+            || arg.lastIndexOf("Requiring files:", 0) === 0
+            || arg.lastIndexOf("Requiring page code:", 0) === 0
+            || arg.lastIndexOf("initialize -", 0) === 0) {
+
+            lastBindingSeenTime = new Date().getTime();
+
+            if (!runnerCheckerIsStarted) {
+              taskId = window.setInterval(runnerChecker, 100);
+              runnerCheckerIsStarted = true;
+            }
+          }
+
+          if (original.apply) {
+            // Do this for normal browsers
+            original.apply(console, arguments);
+          } else {
+            // Do this for IE
+            var message = Array.prototype.slice.apply(arguments).join(' ');
+            original(message);
+          }
+        };
+      }
+
+      var methods = ['log', 'warn', 'error'];
+      for (var i = 0; i < methods.length; i++)
+        intercept(methods[i]);
+    }
+
+    interceptConsole();
+
     require(['/-/speak/v1/assets/main.js'], function () {
       require(['tests', 'jquery'], function (tests, $) {
         if (!tests) {
           return;
         }
 
-        var lastBindingSeenTime = 0;
-        var bindingSeenTimeDelta = 0;
-        var runnerIsStarted = false;
-
-        var taskId = 0;
-
-        function testRunner() {
-          bindingSeenTimeDelta = new Date().getTime() - lastBindingSeenTime;
-
-          if (bindingSeenTimeDelta > 2000) {
-            window.clearInterval(taskId);
-
-            console.log('Running tests.');
-
-            if (window.mochaPhantomJS) {
-              mochaPhantomJS.stats = { fail: 0, pass: 0, total: 0 };
-              mochaPhantomJS.run();
-            } else {
-              require(['loca'], function () {
-                mocha.reporter(mocha.WebKit);
-                mocha.run();
-              });
-            }
+        testRunner = function () {
+          console.log('Running tests.');
+          if (window.mochaPhantomJS) {
+            mochaPhantomJS.stats = { fail: 0, pass: 0, total: 0 };
+            mochaPhantomJS.run();
+          } else {
+            require(['loca'], function () {
+              mocha.reporter(mocha.WebKit);
+              mocha.run();
+            });
           }
-        }
-
-        function interceptConsole() {
-          var console = window.console;
-          if (!console) return;
-          function intercept(method) {
-            var original = console[method];
-            console[method] = function () {
-              var arg = arguments[0].toString();
-              if (arg.lastIndexOf("Applying Binding for the element", 0) === 0
-                || arg.lastIndexOf("Requiring files:", 0) === 0
-                || arg.lastIndexOf("Requiring page code:", 0) === 0
-                || arg.lastIndexOf("initialize -", 0) === 0) {
-
-                lastBindingSeenTime = new Date().getTime();
-
-                if (!runnerIsStarted) {
-                  taskId = window.setInterval(testRunner, 100);
-                  runnerIsStarted = true;
-                }
-              }
-
-              if (original.apply) {
-                // Do this for normal browsers
-                original.apply(console, arguments);
-              } else {
-                // Do this for IE
-                var message = Array.prototype.slice.apply(arguments).join(' ');
-                original(message);
-              }
-            };
-          }
-
-          var methods = ['log', 'warn', 'error'];
-          for (var i = 0; i < methods.length; i++)
-            intercept(methods[i]);
-        }
-
-        interceptConsole();
+        };
       });
     });
   });
