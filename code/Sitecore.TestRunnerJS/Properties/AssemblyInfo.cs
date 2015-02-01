f4627620586a7fa7665a6a58ab610a99f99b2aae
Yuriy Morozov <yury.morozov@gmail.com>
2015-02-01 22:09:50 +0200

    Add creation of nuget package

--- code/Sitecore.TestRunnerJS/Properties/AssemblyInfo.cs
+++ code/Sitecore.TestRunnerJS/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.TestRunnerJS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Javascript test runner")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yuriy Morozov")]
 [assembly: AssemblyProduct("Sitecore.TestRunnerJS")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
