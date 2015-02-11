3eec0430d7b53968dcfc0a73adc35abec56a7358
Yuriy Morozov <yum@sitecore.net>
2015-02-11 17:43:29 +0200

    Fix issue with routes registration on newest Sitecore. Use initialize pipeline instead of WebActivator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
-﻿0.6.0
+﻿0.6.1
+* [FIX] Fix issue with routes registration on newest Sitecore. Use initialize pipeline instead of WebActivator
+
+0.6.0
 * Initial public release with nuget package
---
+++ code/Sitecore.TestRunnerJS/Pipelines/Initialize/RegisterTestRunnerRoutes.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.TestRunnerJS.Pipelines.Initialize
+{
+  using System.Web.Http;
+  using Sitecore.Pipelines;
+
+  public class RegisterTestRunnerRoutes
+  {
+    public void Process(PipelineArgs args)
+    {
+      GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+        "TestFixture",
+        "TestRunnerJS/testfixture/{action}",
+        new { controller = "TestFixture" });
+    }
+  }
+}
