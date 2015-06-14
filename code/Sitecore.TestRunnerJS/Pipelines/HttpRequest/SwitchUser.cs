4a622333e769e40339f3c5a72370fde07271846e
Yuriy Morozov <yury.morozov@gmail.com>
2015-06-15 00:40:24 +0300

    Add simple processor for user switching

---
+++ code/Sitecore.TestRunnerJS/Pipelines/HttpRequest/SwitchUser.cs
@@ -0,0 +1,13 @@
+﻿namespace Sitecore.TestRunnerJS.Pipelines.HttpRequest
+{
+  using Sitecore.Pipelines.HttpRequest;
+  using Sitecore.Security.Accounts;
+
+  public class SwitchUser : HttpRequestProcessor
+  {
+    public override void Process(HttpRequestArgs args)
+    {
+      var switcher = new UserSwitcher("sitecore\\admin", true);
+    }
+  }
+}
